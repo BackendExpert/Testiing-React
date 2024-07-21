74a6feb25a1cfbe73f5af916c68eba7a05369076
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-21 19:07:18 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/TestingCom/Forms/SignUpForm.jsx
+++ client/src/TestingCom/Forms/SignUpForm.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import BgImg from '../../assets/Image.jpg'
 
 const SignUpForm = ({title}) => {
+    const styles = {
+        background: `url(${BgImg})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    }
   return (
-    <div>{title}</div>
+    <div style={styles} className='bg-gradient-to-r from-violet-500 to-purple-500 min-h-screen py-24 px-8'>
+
+    </div>
   )
 }
 
